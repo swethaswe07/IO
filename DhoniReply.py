addd8adfe385c6eca3fc51e2e91434647d0b4fa1
Swetha P V <swethapv03@gmail.com>
2024-09-19 14:58:37 +0530

    Update and rename DhoniReply to DhoniReply.py

---
+++ DhoniReply.py
@@ -0,0 +1,18 @@
+'''
+After Dhoni made it to the pinnacle of Success as Indian Captain, Mr. Banerjee was once invited by the Media to recall his association with Dhoni. Mr. Banerjee quoted one important moment that complemented the traits of Dhoni. It was when he approached Dhoni and asked him if he would play cricket for the school team saying "Will you be a wicketkeeper?" and was taken aback by Dhoni's confident reply "I will if I get a chance". Write a program to display the answers of Dhoni that impressed his master.
+Input and Output Format:
+Refer Sample input and output for formatting specifications.
+All text in bold corresponds to input and the rest corresponds to output.
+'''
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
+
+
+'''
+output
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
+
+'''
