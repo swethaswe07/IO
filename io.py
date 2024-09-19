9fdcad9d477fed32268a5ee2e80c72974b9cd642
Swetha P V <swethapv03@gmail.com>
2024-09-19 12:29:23 +0530

    Add files via upload
    1)MS Dhoni aged X years, is a Cancerian, born with very strong Mars in his birth chart. Notably, Mars is the ruling planet for sports. Write a program to get the age of Dhoni as an integer and display the same.

---
+++ io.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter the age:\n",
+      "35\n",
+      "Age of Dhoni is 35\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Program to get the age of MS Dhoni\n",
+    "\n",
+    "# Prompt user for input\n",
+    "age = int(input(\"Enter the age:\\n\"))\n",
+    "\n",
+    "# Display the output\n",
+    "print(f\"Age of Dhoni is {age}\")\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Program to display Dhoni's reply to Banerjee's question\n",
+    "\n",
+    "# Get input from the user\n",
+    "p = input(\"Banerjee's Question:\\n\")\n",
+    "r = input(\"Dhoni's Reply:\\n\")\n",
+    "\n",
+    "# Display the formatted output\n",
+    "print(\"For Banerjee's question \\\"{}\\\" Dhoni's confident reply was \\\"{}\\\".\".format(p, r))\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
